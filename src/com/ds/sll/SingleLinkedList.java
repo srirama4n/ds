b337074e4a8842df1ca887d6b2e35ca3530b71fa
srirama4n <srirama4n@gmail.com>
2018-02-15 23:47:14 +0530

    Create a single list and print all the values

--- src/com/ds/sll/SingleLinkedList.java
+++ src/com/ds/sll/SingleLinkedList.java
@@ -1,4 +1,36 @@
 package com.ds.sll;
 
 public class SingleLinkedList {
+
+    Node head;
+    Node tail;
+
+    public void printAll() {
+        Node node = head;
+        while (node != null) {
+            System.out.println("Node data :  " + node.data);
+            node = node.next;
+        }
+    }
+
+    public void addLast(int data) {
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+}
+
+class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
 }
--- src/com/ds/sll/SingleLinkedListTest.java
+++ src/com/ds/sll/SingleLinkedListTest.java
@@ -1,4 +1,17 @@
 package com.ds.sll;
 
 public class SingleLinkedListTest {
+
+    public static void main(String[] args) {
+        validateAdd();
+    }
+
+    static void validateAdd() {
+        SingleLinkedList sll = new SingleLinkedList();
+        sll.addLast(1);
+        sll.addLast(2);
+        sll.addLast(3);
+        sll.addLast(4);
+        sll.printAll();
+    }
 }
